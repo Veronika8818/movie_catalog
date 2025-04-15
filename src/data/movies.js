f3a65337223b42e9a7cb1d91df4718619d356344
Veronika <veronika.korol2020@gmail.com>
2025-04-15 13:14:43 +0300

    Додано перелік фільмів

---
+++ src/data/movies.js
@@ -0,0 +1,40 @@
+export const movies = [
+    {
+      id: 1,
+      title: "Оппенгеймер",
+      description: "Світ назавжди зміниться.",
+      genre: "Драма, Історичний",
+      poster: "https://uaserials.pro/posters/8215.webp",
+      date: "2025-04-15",
+      time: "18:30"
+    },
+    {
+      id: 2,
+      title: "Ущелина",
+      description: "Вони не мають права на помилку, бо на кону тепер існування всього людства…",
+      genre: "Фантастика, Драма",
+      poster: "https://uaserials.pro/posters/10004.webp",
+      date: "2025-04-15",
+      time: "20:00"
+    },
+    {
+        id: 3,
+        title: "Фантастичні звірі: Таємниці Дамблдора",
+        description: "Третя частина франшизи 'Фантастичні звірі' - таємниці Дамблдора",
+        genre: "Фантастика",
+        poster: "https://uakino.me/uploads/posts/2022-05/1652471227_1652471222_624c685815177.jpg",
+        date: "2025-01-11",
+        time: "22:00"
+      },
+      {
+        id: 4,
+        title: "Крик 6",
+        description: "Він повертається...",
+        genre: "Жахи",
+        poster: "https://uakino.me/uploads/mini/poster/c1/702fc0d84026dbab82fb3c4afcec09.webp",
+        date: "2025-02-21",
+        time: "13:30"
+      },
+  ];
+  
+  
